b1435890f44f006a5eaec03d98aa0029a2c528e9
hemanta212 <sharmahemanta.212@gmail.com>
2022-10-17 10:14:07 +0545

    Chap 4.2 Verification of inputs

--- login.gtpl
+++ login.gtpl
@@ -5,7 +5,20 @@
 <body>
 <form action="/login" method="post">
 Username:<input type="text" name="username">
+Age:<input type="number" name="age">
 Password:<input type="password" name="password">
+<select name="fruit">
+  <option value="apple"> apple </option>
+  <option value="pear"> pear </option>
+  <option value="banana"> banana </option>
+</select>
+<input type="radio" name="gender" value="1"> Male
+<input type="radio" name="gender" value="2"> Female
+
+<input type="checkbox" name="interest" value="football"> Football
+<input type="checkbox" name="interest" value="cricket"> Cricket
+<input type="checkbox" name="interest" value="tennis"> Tennis
+
 <input type="submit" value="Login">
 </form>
 </body>
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -12,17 +13,68 @@ func login(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
+
 		fmt.Println("username: ", r.Form["username"])
+		fmt.Println("age: ", r.Form["age"])
 		fmt.Println("password: ", r.Form["password"])
+		fmt.Println("fruits: ", r.Form["fruit"])
+		fmt.Println("gender: ", r.Form["gender"])
+		fmt.Println("Interests: ", r.Form["interest"])
 		// Tip we can use r.FormValue("username") instead which
 		// automatically calls ParseForm
 		// downside: silences errors when key not found returning ""
 		// and if multiple value present returns only first one.
+
+		if len(r.Form["username"][0]) == 0 {
+			r.Method = "GET"
+			login(w, r)
+		}
+		getint, _ := strconv.Atoi(r.Form["age"][0])
+		if getint <= 0 || getint > 100 {
+			r.Method = "GET"
+			login(w, r)
+		}
+		if !validateFruits(r) {
+			r.Method = "GET"
+			login(w, r)
+		}
+		if !validateMaleFemale(r) {
+			r.Method = "GET"
+			login(w, r)
+		}
+
+		// checkbox validation is different, should do a Set Diffrentiation A-B, and check for nil
+		interests := []string{"football", "cricket", "tennis"}
+		// slice_diff := Slice_diff(r.Form["interest"], interests)
+		// if slice_diff == nil { return true}
+		// return false
+
 	}
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world")
+}
+
+func validateFruits(r *http.Request) bool {
+	fruits := []string{"apple", "banana", "pear"}
+	for _, v := range fruits {
+		if v == r.Form.Get("fruit") {
+			return true
+		}
+	}
+	return false
+}
+
+func validateMaleFemale(r *http.Request) bool {
+	maleFemale := []int{1, 2}
+	for _, v := range maleFemale {
+		gender, _ := strconv.Atoi(r.Form.Get("gender"))
+		if v == gender {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
